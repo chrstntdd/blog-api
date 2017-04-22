231d67f34fbe4d0f641624d696b417521f3576d1
Christian Todd <christiantodd27@icloud.com>
2017-04-22 10:22:14 -0400

    abstract check for params into separate function.

--- blogRouter.js
+++ blogRouter.js
@@ -10,6 +10,16 @@ BlogPosts.create('Foo', 'Whatever', 'Kenny Rodgers', '4/21/1963');
 BlogPosts.create('Bar', 'Lorem', 'Dick Fittsmore', '8/29/2015');
 BlogPosts.create('Yup', 'Ipsum', 'Moe Lester', '3/15/2001');
 
+checkFields = (requiredFields, req) => {
+  requiredFields.map(field => {
+    if (!(field in req.body)){
+      const message = `Missing ${field} in the request body.`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  });
+}
+
 router.get('/', (req, res) => {
   res.json(BlogPosts.get());
 });
@@ -17,13 +27,7 @@ router.get('/', (req, res) => {
 router.post('/', jsonParser, (req, res) => {
   // check if required fields are filled out
   let requiredFields = ['title', 'content', 'author'];
-  requiredFields.map(field => {
-    if (!(field in req.body)) {
-      const message = `Missing ${field} in request body`;
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  });
+  checkFields(requiredFields, req);
   let post = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
   res.status(201).json(post);
 });
